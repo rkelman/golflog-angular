31a70300cc0082476de1331f3255770ad4356938
Robb Kelman <robb.kelman@gmail.com>
2018-04-12 15:22:11 -0700

    copied all listing components from report to listing

--- src/app/listing/listing.component.ts
+++ src/app/listing/listing.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { UserService } from '../user.service';
+import { ActivityService } from '../activity.service';
+import { AlertService } from '../alert.service';
+import { Observable, Subscription } from "rxjs";
 
 @Component({
   selector: 'app-listing',
@@ -6,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listing.component.css']
 })
 export class ListingComponent implements OnInit {
-
-  constructor() { }
+  currentUser: User;
+  activities = [];
+  message: {success: boolean, 
+    msg: string};
+    
+  constructor(private activityService: ActivityService,
+    private alertService: AlertService) { }
 
   ngOnInit() {
+    this.getList();
+  }
+
+  deleteActivity(activityID) {
+    this.activityService.deleteActivity(activityID, this.currentUser.uid)
+    .subscribe(data => this.message = data);
+
+    this.getList();
+  }
+
+  getList(): void{
+    this.activityService.getActivityList(this.currentUser.uid, this.currentUser.token, 10)
+      .subscribe(data => this.activities = data);
   }
 
 }
