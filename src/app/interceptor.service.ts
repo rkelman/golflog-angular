85847653732c28decdc4bee2a67ee039761c7c06
Robb Kelman <robb.kelman@gmail.com>
2018-06-04 18:36:15 -0700

    added currentUser token to Header

--- src/app/interceptor.service.ts
+++ src/app/interceptor.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor } from '@angular/common/http';
+import { User } from '../app/user';
+
 
 @Injectable()
 export class InterceptorService implements HttpInterceptor {
+  currentUser: User;
 
-  constructor() { }
+  constructor() {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
 
   intercept(req, next) {
     let tokenizedReq = req.clone({
       setHeaders: {
-        Auth: 'Bearer xx.yy.zz'
+        Auth: 'Bearer '+this.currentUser.token
       }
     })
     return next.handle(tokenizedReq)
