18a6705ea0692f26ca496df5daf9665c1e4f53bc
Robb Kelman <robb.kelman@gmail.com>
2018-06-09 02:12:53 -0700

    [0.1.10] updated report chart to update r/t new, refactored

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -17,42 +17,35 @@ export class ReportComponent implements OnInit {
   periods = [];
   activities = [];
   summary = [];
-  //summary = {type: String, elapsedTime: String}[];
   message: {success: boolean, 
     msg: string};
-  chart = [];
+  chart: Chart;
 
   constructor(private activityService: ActivityService,
               private alertService: AlertService) { 
       this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
       this.message = {success: true, 
-        msg: ""};
+        msg: ""};     
   }
 
   ngOnInit() {
     this.periods = ["week", "month", "quarter", "year"];
     //this.getList();
     this.getSummary("month");
+    this.chart = new Chart('canvas', {
+      type: 'pie',
+      data: {},
+      options: {
+        title: {
+          display: true,
+          text: 'Your Golf Practice Summary'
+        }
+      }
+    });
   }
 
-  /*
-  deleteActivity(activityID) {
-    this.activityService.deleteActivity(activityID, this.currentUser.uid)
-    .subscribe(data => this.message = data);
-
-    this.getList();
-
-    //this.getSummary();
-  }*/
-
-  /*
-  getList(): void{
-    this.activity,
-    Service.getActivityList(this.currentUser.uid, this.currentUser.token, 10)
-      .subscribe(data => this.activities = data);
-  }*/
-
   getSummary(period) {
+
     this.activityService.getActivitySummary(this.currentUser.uid, this.currentUser.token, period)
       .subscribe(data => this.summary = data);
 
@@ -66,64 +59,15 @@ export class ReportComponent implements OnInit {
           //console.log('Type '+i+': '+this.summary[i].type);
         }
         
-        this.chart = [];
-        this.chart = new Chart('canvas', {
-          type: 'pie',
-          data: {
-            labels: chartjsLabels,
-            datasets: [{
-              backgroundColor: ["#3e95cd", "#ffcc00","#3cba9f"],
-              data: chartjsData, 
-              //labels: chartjsDataLabels
-            }]
-          },
-          options: {
-            title: {
-              display: true,
-              text: 'Your Golf Practice Summary'
-            }
-          }
-        });
-
+        this.chart.data = {
+          labels: chartjsLabels,
+          datasets: [{
+            backgroundColor: ["#3e95cd", "#ffcc00","#3cba9f"],
+            data: chartjsData,
+            labels: chartjsLabels
+          }]
+        };
+        this.chart.update();
       });
-/*
-    let chartjsLabels = [];
-    for (var i = 0; i < this.summary.length; i++) {
-      chartjsLabels.push(this.summary[i].type);  
-      //console.log('Type '+i+': '+this.summary[i].type);
-    }
-    //chartjsLabels = ["Putting", "Chipping", "Full-Swing", "Approach"];
-    
-    let chartjsData = [];
-    for (var i = 0; i < this.summary.length; i++) {
-      chartjsData.push(this.summary[i].sumRawTime);  
-    }
-
-    for (var i = 0; i < this.summary.length; i++) {
-      chartjsData.push(this.summary[i].elapsedTime);  
-    }
-    let chartjsDataLabels = [];
-    //chartjsData = ["14", "5", "5", "4"];
-    console.log('Labels: '+chartjsLabels);
-    console.log('Data: '+chartjsData);
-    console.log('Data Labels: '+chartjsDataLabels);
-
-    this.chart = new Chart('canvas', {
-      type: 'pie',
-      data: {
-        labels: chartjsLabels,
-        datasets: [{
-          backgroundColor: ["#3e95cd", "#ffcc00","#3cba9f"],
-          data: chartjsData, 
-          labels: chartjsDataLabels
-        }]
-      },
-      options: {
-        title: {
-          display: true,
-          text: 'Your Golf Practice Summary'
-        }
-      }
-    });*/
   }
 }
